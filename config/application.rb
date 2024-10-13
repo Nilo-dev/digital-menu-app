1c80ef8406cc0abf8b452658624ce29eaddb1730
nilo-dev <danilo.dalo.nilo@gmail.com>
2024-10-13 15:36:00 -0300

    Configurating application to ignore crsf token

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module DigitalMenuApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Ignoring CRSF Token for development purposes
+    config.action_controller.default_protect_from_forgery = false
   end
 end
