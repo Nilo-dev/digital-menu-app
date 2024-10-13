2d8d37bb9f4e513988fc9a58b98b960593f9382a
nilo-dev <danilo.dalo.nilo@gmail.com>
2024-10-13 16:09:01 -0300

    Implementing methods in menus controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,21 @@
 class MenusController < ApplicationController
   def index
+    response = MenuService.fetch_menus
+    render json: response, status: response[:status]
   end
 
   def show
+    response = MenuService.fetch_menu(params[:id])
+    render json: response, status: response[:status]
+  end
+
+  def create
+    response = MenuService.create_menu(params[:name])
+    render json: response, status: response[:status]
+  end
+
+  def update
+    response = MenuService.update_menu(id: params[:id], name: params[:name])
+    render json: response, status: response[:status]
   end
 end
