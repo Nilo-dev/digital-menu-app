304d09dfd8d913391078d2b417a936e936b9e1d2
nilo-dev <danilo.dalo.nilo@gmail.com>
2024-10-14 10:36:13 -0300

    Removing duplicate fields and ensuring uniqueness of name field

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,5 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu
 
-  validates :name, presence: true
-  validates :price, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_14_131642) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_133354) do
   create_table "menu_item_menus", force: :cascade do |t|
     t.integer "menu_id", null: false
     t.integer "menu_item_id", null: false
@@ -24,12 +24,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_131642) do
 
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
-    t.string "description"
-    t.decimal "price"
-    t.integer "menu_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+    t.index ["name"], name: "index_menu_items_on_name", unique: true
   end
 
   create_table "menus", force: :cascade do |t|
@@ -48,6 +45,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_131642) do
 
   add_foreign_key "menu_item_menus", "menu_items"
   add_foreign_key "menu_item_menus", "menus"
-  add_foreign_key "menu_items", "menus"
   add_foreign_key "menus", "restaurants"
 end
