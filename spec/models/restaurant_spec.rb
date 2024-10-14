7d77b20fd07bf0d7d3e4672a5284d697d10456e3
nilo-dev <danilo.dalo.nilo@gmail.com>
2024-10-14 18:28:14 -0300

    Creating restaurant model specs

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:menus).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'database columns' do
+    it { should have_db_column(:name).of_type(:string) }
+  end
 end
