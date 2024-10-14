ae7ae8387769be44db2ec6268e46bbdcf9a84c9b
nilo-dev <danilo.dalo.nilo@gmail.com>
2024-10-14 19:02:43 -0300

    Fixing menu item specs

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MenuItem, type: :model do
-  let(:menu_item) { create(:menu_item) }
+  let(:menu_item) { build(:menu_item) }
 
   describe 'validations' do
-    it 'is valid with a name, price, and description' do
+    it 'is valid with a name' do
       expect(menu_item).to be_valid
     end
 
@@ -14,19 +14,16 @@ RSpec.describe MenuItem, type: :model do
       expect(menu_item.errors[:name]).to include("can't be blank")
     end
 
-    it 'is invalid without a price' do
-      menu_item.price = nil
+    it 'is invalid if the name is not unique' do
+      create(:menu_item, name: 'Unique Item')
+      menu_item.name = 'Unique Item'
       expect(menu_item).not_to be_valid
-      expect(menu_item.errors[:price]).to include("can't be blank")
-    end
-
-    it 'is invalid without a description' do
-      menu_item.description = nil
-      expect(menu_item).to be_valid
+      expect(menu_item.errors[:name]).to include("has already been taken")
     end
   end
 
   describe 'associations' do
-    it { should belong_to(:menu) }
+    it { should have_many(:menu_item_menus).dependent(:destroy) }
+    it { should have_many(:menus).through(:menu_item_menus) }
   end
 end
